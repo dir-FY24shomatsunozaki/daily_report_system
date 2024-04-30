d18808260e70f7745d3feb367c5c18f415fabbaa
dir-FY24shomatsunozaki <shoma.tsunozaki.t825@dirbato.co.jp>
2024-04-30 14:58:08 +0900

    ログアウトアクションの作成

--- daily_report_system/src/main/java/listeners/PropertiesListener.java
+++ daily_report_system/src/main/java/listeners/PropertiesListener.java
@@ -1,35 +1,55 @@
 package listeners;
 
-import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
 
-/**
- * Application Lifecycle Listener implementation class PropertiesListener
- *
- */
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
 @WebListener
-public class PropertiesListener implements HttpSessionListener {
+public class PropertiesListener implements ServletContextListener {
+
+    public PropertiesListener() {
+    }
 
     /**
-     * Default constructor. 
+     * Webアプリケーションのシャットダウン時に実行する処理
      */
-    public PropertiesListener() {
-        // TODO Auto-generated constructor stub
+    public void contextDestroyed(ServletContextEvent arg0) {
     }
 
-	/**
-     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+    /**
+     * Webアプリケーションの起動時に実行する処理
      */
-    public void sessionCreated(HttpSessionEvent se)  { 
-         // TODO Auto-generated method stub
+    public void contextInitialized(ServletContextEvent arg0) {
+        ServletContext context = arg0.getServletContext();
+
+        //プロパティファイルを読み込み、アプリケーションスコープに設定する
+        try {
+            InputStream is = PropertiesListener.class.getClassLoader().getResourceAsStream("application.properties");
+
+            Properties properties = new Properties();
+            properties.load(is);
+            is.close();
+
+            Iterator<String> pit = properties.stringPropertyNames().iterator();
+            while (pit.hasNext()) {
+                String pname = pit.next();
+                context.setAttribute(pname, properties.getProperty(pname));
+
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-	/**
-     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
-     */
-    public void sessionDestroyed(HttpSessionEvent se)  { 
-         // TODO Auto-generated method stub
-    }
-	
 }
